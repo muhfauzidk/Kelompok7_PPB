13d74f76f3f8d1ddb88ae8f7ab1b929f2c110fbc
muhfauzidk <86062100+muhfauzidk@users.noreply.github.com>
2022-04-24 03:41:45 +0700

    Add files via upload

---
+++ lib/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+import 'package:shop_app/screens/cart/cart_screen.dart';
+import 'package:shop_app/screens/details/details_screen.dart';
+import 'package:shop_app/screens/home/home_screen.dart';
+import 'package:shop_app/screens/profile/profile_screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  HomeScreen.routeName: (context) => HomeScreen(),
+  DetailsScreen.routeName: (context) => DetailsScreen(),
+  CartScreen.routeName: (context) => CartScreen(),
+  ProfileScreen.routeName: (context) => ProfileScreen(),
+};
